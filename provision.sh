1e5506c3f67f0389f395d2be5e43f799ee44c0c8
Liam Norton <liam.norton@flippa.com>
2016-02-02 22:13:02 +1100

    Add ruby 2.3.0 and phantomjs

--- provision.sh
+++ provision.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
 
-sudo sh <<EOF
+sudo \
+  BUNDLER_VER="1.11.2" \
+  PHANTOM_JS="phantomjs-1.9.8-linux-x86_64" \
+  RUBY_VER="2.3.0" \
+  sh <<'EOF'
 DEBIAN_FRONTEND=noninteractive \
-  apt-get update && apt-get -y upgrade &&
-  apt-get -y --force-yes --no-install-recommends install \
-    ack-grep \
-    build-essential \
-    git-core \
-    libpq-dev \
-    libxml2-dev \
-    libxslt1-dev \
-    nodejs \
-    postgresql \
-    postgresql-client \
-    postgresql-contrib \
-    ruby-dev \
-    vim
+apt-get update && apt-get -y upgrade &&
+apt-get -y --force-yes --no-install-recommends install \
+  ack-grep \
+  build-essential \
+  chrpath \
+  git-core \
+  libc-dev \
+  libfreetype6-dev \
+  libfontconfig1-dev \
+  libssl-dev \
+  libpq-dev \
+  libxml2-dev \
+  libxft-dev \
+  libxslt1-dev \
+  nodejs \
+  postgresql \
+  postgresql-client \
+  postgresql-contrib \
+  ruby \
+  vim
 
-gem install bundler --no-ri --no-rdoc --version 1.11.2
+
+git clone https://github.com/postmodern/ruby-install /usr/src/ruby-install
+cd /usr/src/ruby-install
+make install
+ruby-install --system ruby ${RUBY_VER} -- --disable-install-rdoc
+apt-get purge ruby
+rm -rf /usr/src/*
+rm -rf /var/lib/apt/lists/*
+
+cd /tmp
+wget https://bitbucket.org/ariya/phantomjs/downloads/${PHANTOM_JS}.tar.bz2
+tar xvjf ${PHANTOM_JS}.tar.bz2
+mv ${PHANTOM_JS} /usr/local/share
+ln -sf /usr/local/share/${PHANTOM_JS}/bin/phantomjs /usr/local/bin
+
+gem install bundler --no-ri --no-rdoc --version ${BUNDLER_VER}
 bundle config --global build.nokogiri --use-system-libraries
 
 dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
