e14f36c728f5583a7c53972411b34414c658e600
Liam Norton <liam.norton@flippa.com>
2016-03-01 19:16:39 -0800

    Heroku toolbelt install
    Continuously deploying master now anyway

--- provision.sh
+++ provision.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 sudo \
   BUNDLER_VER="1.11.2" \
@@ -20,13 +20,13 @@ apt-get -y --force-yes --no-install-recommends install \
   libxml2-dev \
   libxft-dev \
   libxslt1-dev \
+  make \
   nodejs \
   postgresql \
   postgresql-client \
   postgresql-contrib \
   ruby \
   vim
-
 
 git clone https://github.com/postmodern/ruby-install /usr/src/ruby-install
 cd /usr/src/ruby-install
@@ -38,7 +38,7 @@ rm -rf /var/lib/apt/lists/*
 
 cd /tmp
 wget https://bitbucket.org/ariya/phantomjs/downloads/${PHANTOM_JS}.tar.bz2
-tar xvjf ${PHANTOM_JS}.tar.bz2
+tar xjf ${PHANTOM_JS}.tar.bz2
 mv ${PHANTOM_JS} /usr/local/share
 ln -sf /usr/local/share/${PHANTOM_JS}/bin/phantomjs /usr/local/bin
 
@@ -47,10 +47,18 @@ bundle config --global build.nokogiri --use-system-libraries
 
 dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
 
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
 echo heroku-vm > /etc/hostname
 EOF
 
 ln -snf /vagrant/projects ~/projects
+
+echo "Done."
+echo ""
+echo "Post install instructions:"
+echo "sudo -u postgres psql postgres"
+echo "\password postgres"
 
 cat > ~/.bashrc <<'EOF'
 export EDITOR=vim
